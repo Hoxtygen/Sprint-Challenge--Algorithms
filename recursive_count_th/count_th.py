c68f321c2cb64f2d7f531428d84addc06a79449d
Hoxtygen <hoxtygen@live.com>
2019-10-11 12:35:12 +0100

    Refactor recursive count

--- recursive_count_th/count_th.py
+++ recursive_count_th/count_th.py
@@ -3,16 +3,13 @@ Your function should take in a signle parameter (a string `word`)
 Your function should return a count of how many occurences of ***"th"*** occur within `word`. Case matters.
 Your function must utilize recursion. It cannot contain any loops.
 '''
-
-
 def count_th(word):
-    wordLen = len(word)
-    if wordLen == 0 or wordLen < 2:
+    
+    # TBC
+    i = word.find('th') # Look for the first occurence of `th`
+    if i == -1: # if no occurrence return 0 (recursive base case)
         return 0
-    elif word[0:2] == "th":
-        return count_th(word[1:]) + 1
-    else:
-        return count_th(word[1:])
+    return 1 + count_th(word[i+2:])
 
 
 print(count_th("abcthefthghith"))
