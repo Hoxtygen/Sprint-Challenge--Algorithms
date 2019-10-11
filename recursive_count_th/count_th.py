a2987a5eb58bb07ea69b0995dc7cfdbb3534ce88
Hoxtygen <hoxtygen@live.com>
2019-10-11 11:07:49 +0100

    Solve robot sort

--- recursive_count_th/count_th.py
+++ recursive_count_th/count_th.py
@@ -4,7 +4,8 @@ Your function should return a count of how many occurences of ***"th"*** occur w
 Your function must utilize recursion. It cannot contain any loops.
 '''
 def count_th(word):
-    
-    # TBC
+    if len(word) == 0:
+        return 0
+    count = 0
     
     pass
